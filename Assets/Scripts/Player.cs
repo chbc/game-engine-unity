d0ec1f0ea53b74dccf17be9aed31fd9333a8cff6
Henrique <henrique.dsc@gmail.com>
2024-05-13 02:45:02 -0300

    Added ship logic.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform _transform;
+    public float _speed;
+    public float _horizontalSpeed;
+    public float _verticalSpeed;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = _transform.position;
+        newPosition.z = newPosition.z + (_speed * Time.deltaTime);
+
+        float xDirection = Input.GetAxis("Horizontal");
+        float yDirection = Input.GetAxis("Vertical");
+
+        newPosition.x = newPosition.x + (xDirection * _horizontalSpeed * Time.deltaTime);
+        newPosition.y = newPosition.y + (yDirection * _verticalSpeed * Time.deltaTime);
+
+        _transform.position = newPosition;
+
+
     }
 }
 
